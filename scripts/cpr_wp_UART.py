0aa0e9e35d916a5d828674904050c50001fa4f53
HyperBlitz711 <doylemd2005@gmail.com>
2026-03-24 17:42:43 -0300

    add right side files

--- scripts/cpr_wp_UART.py
+++ scripts/cpr_wp_UART.py
@@ -32,12 +32,12 @@ class NavigatorNode(Node):
         # Declare routes
         self.inspection_route = [
             #[f2m(0.0), f2m(3.0) -f2m(13.5/12.0), 0.0] # up for testing
-            [f2m(6.0), f2m(3.0) -f2m(13.5/12.0), 0.0],
+            [f2m(6.0), -f2m(3.0) +f2m(13.5/12.0), 0.0],
             [f2m(9.0), f2m(0.0), 0.0],
             #[f2m(12.5), f2m(2.0), -1.57], # Aim at cargo
             #[f2m(12.5), -f2m(0.5), -1.57], # Ram cargo (Hopefully acquire)
             #[f2m(11.6), -f2m(0.5), 3.14] # Look away to dump 1st cargo
-            [f2m(11.6), -f2m(0.2), 3.14] # Ram and turn, now we can dump
+            [f2m(11.6), f2m(0.2), 3.14] # Ram and turn, now we can dump
         ]
         self.reverse_route = [
             [f2m(6.0), f2m(3.0) -f2m(13.5/12.0), 3.14],
